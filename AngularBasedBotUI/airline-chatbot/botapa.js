b4f3f8c45452ae28eef9f8d4b3a4b7b2b44bffeb
CodeCraftsman <jayswalkuber.gmail.com>
2024-12-02 23:33:45 -0500

    updated

--- AngularBasedBotUI/airline-chatbot/botapa.js
+++ AngularBasedBotUI/airline-chatbot/botapa.js
@@ -1,5 +1,5 @@
 import OpenAI from "openai";
-const openai = new OpenAI({apiKey: "3b1aac713e2430c5", baseURL: "https://hexavarsity-secureapi.azurewebsites.net/api/secure-ai"});
+const openai = new OpenAI({apiKey: "3b1aac713e2430c5", baseURL: "https://hexavarsity-secureapi.azurewebsites.net/api/azureai"});
 async function main() {
     try {
         const completion = await openai.chat.completions.create({
@@ -7,9 +7,9 @@ async function main() {
                 { role: "system", content: "You are an airline information assistant. Provide accurate and helpful information related to flight status, booking details, baggage policies, and other airline-related inquiries." },
                 { role: "user", content: "Hello" }
             ],
-            model: "gpt-4", // Specify the model
+            model: "gpt-4o", // Ensure this is the correct model name
             temperature: 0.7,
-            max_tokens: 1000,
+            max_tokens: 256,
             top_p: 0.6,
             frequency_penalty: 0.7
         });
